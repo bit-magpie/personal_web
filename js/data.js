08ee3e8f601c7ea37dbc875719c579561ec30e4e
Isuru Jayarathne <isuru.jayarathne@gmail.com>
2023-01-03 20:13:40 +0900

    capitalized some texts

--- js/data.js
+++ js/data.js
@@ -68,31 +68,31 @@ const eduInfo = [
 const expInfo = [
     {
         institute: "University of Aizu, Japan",
-        position: "Special research staff",
+        position: "Special Research Staff",
         resposibility: "My current research focus is on analyzing 3D point clouds for the purpose of indoor mobile robot path planning. This work involves using computer simulations to generate point cloud data, which is then processed using various libraries. The end goal of this research is to improve the ability of indoor mobile robots to navigate through complex environments. Tools and technologies: PCL (Point Cloud Library), Choreonoid, ROS",
         period: "(April 2021 - Present)",
     },
     {
         institute: "University of Aizu, Japan",
-        position: "Research assistant",
+        position: "Research Assistant",
         resposibility: "While working at the Image Processing Lab under the supervision of Professor Keitaro Naruse, I had the opportunity to participate in a research project focused on developing an indoor, multi-robot path planning algorithm. This project was a collaboration with two robotics companies, and my involvement included contributing to the research efforts to improve the capabilities of indoor multi-robot systems. Tools and technologies: ROS (Robot operating system), Turtlebot3, OpenCV image processing framework, SLAM",
         period: "(April 2020 - March 2021)",
     },
     {
         institute: "University of Aizu, Japan",
-        position: "Research assistant",
+        position: "Research Assistant",
         resposibility: "During my time at the Computer Arts Lab under the supervision of Professor Michael Cohen, I was involved in two research projects. The first project aimed to develop an EEG-based system for identifying and authenticating individuals, while the second project focused on creating a musical relaxation soundscape using reinforcement learning to guide the selection of affective sounds. My contributions to these projects included conducting research and contributing to the progress made in these areas. Tools and technologies: EMOTIV Epoc+, Open BCI, Muse headband, MATLAB(BCILAB, FieldTrip toolboxes), python MNE framework, Keras & Tensorflow frameworks with python",
         period: "(April 2017 - March 2020)",
     },
     {
         institute: "University of Aizu, Japan",
-        position: "Research assistant",
+        position: "Research Assistant",
         resposibility: "As a researcher in the field of Brain-Computer Interfaces and Robotics, my responsibilities have included conducting experiments (primarily related to EEG), analyzing data, writing research papers, and preparing presentations. These activities have allowed me to contribute to the advancement of knowledge in these areas and share my findings with others. Tools and technologies: EMOTIV Epoc+, MATLAB, python",
         period: "(November 2015 - March 2016)",
     },
     {
         institute: "Senior Software Engineer at RS Metrics Asia (Pvt) Ltd.",
-        position: "Senior software engineer",
+        position: "Senior Software Engineer",
         resposibility: "The primary project that I worked on involved creating a system for predicting stock market value using satellite images for the general public. To generate the predictions, I implemented statistical analysis techniques and used manually collected data from satellite images. The aim of the project was to provide the general public with a tool for predicting stock market value using readily available satellite images. Tools and technologies: Yii PHP framework, Java hibernate framework, Angular front-end framework, ORM, Rest API, Microservices",
         period: "(March 2015 - November 2015)",
     },
